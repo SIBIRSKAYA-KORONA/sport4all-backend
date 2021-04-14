d5035dc411ecf8815d4d82a506283cfc60de5242
Тимофей Разумов <timofey.razumov@corp.mail.ru>
2021-04-14 10:25:37 +0300

    fmt

--- app/models/invite.go
+++ app/models/invite.go
@@ -12,10 +12,10 @@ type Invite struct {
 	ID           uint        `json:"id" gorm:"primary_key"`
 	CreatorId    uint        `json:"creator_id" gorm:"not null, index"`
 	InvitedId    *uint       `json:"invited_id,omitempty" gorm:"index"`
-	User         User       `json:"user" gorm:"-"`
+	User         User        `json:"user" gorm:"-"`
 	AssignedId   uint        `json:"assigned_id" gorm:"index"`
 	TeamId       uint        `json:"team_id" gorm:"index"`
-	Team         Team       `json:"team" gorm:"-"`
+	Team         Team        `json:"team" gorm:"-"`
 	TournamentId *uint       `json:"tournament_id,omitempty" gorm:"index"`
 	Type         string      `json:"type"`
 	State        InviteState `json:"state"`
